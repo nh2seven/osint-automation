a9aa30b6e033b177a61826c36b46067e873d1e06
nh2seven <nikhil.h@outlook.com>
2024-03-02 19:21:14 +0530

    Updated IP Info

---
+++ ip_info.py
@@ -0,0 +1,49 @@
+import requests
+
+
+def get_ip_info(ip_address):
+    try:
+        url = f"https://api.facha.dev/v1/ip/{ip_address}"
+        headers = {"User-Agent": "Your User-Agent"}  # Replace with your User-Agent
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+        data = response.json()
+
+        cins_url = "https://cinsscore.com/list/ci-badguys.txt"
+        cins_response = requests.get(cins_url)
+        if ip_address in cins_response.text:
+            data["blacklisted"] = True
+        else:
+            data["blacklisted"] = False
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
+
+
+# Terminal-based output
+print("IP Information Retrieval")
+
+ip_address = input("Enter IP Address:")
+if ip_address:
+    api_data = get_ip_info(ip_address)
+
+    if api_data:
+        ip = api_data["ip"]
+        subnet = api_data["subnet"]
+        asn_number = api_data["asn"]["number"]
+        asn_name = api_data["asn"]["name"]
+        asn_description = api_data["asn"]["description"]
+        country = api_data["country"]
+        hosting = api_data["hosting"]
+        blacklisted = api_data["blacklisted"]
+
+        print("IP Information:")
+        print(f"- IP: {ip}")
+        print(f"- Subnet: {subnet}")
+        print(f"- ASN Number: {asn_number}")
+        print(f"- ASN Name: {asn_name}")
+        print(f"- ASN Description: {asn_description}")
+        print(f"- Country: {country}")
+        print(f"- Hosting: {hosting}")
+        print(f"- Blacklisted: {blacklisted}")
